ad8b69d892c6c2c443ddbab9795687592e6fd392
michaelvuolo <michaelvuolo1@gmail.com>
2022-12-31 18:08:30 -0600

    init implementation for accountingcs gl checker

--- src/utils/tools/bsca/gl-formats/accountingcs.ts
+++ src/utils/tools/bsca/gl-formats/accountingcs.ts
@@ -7,9 +7,11 @@ import type {
   Period,
 } from "@/types/tools/bsca/general-ledger";
 import type { TextShard } from "@/types/ocr";
+import { getTextShardsAsLines } from "@/utils/ocr";
 
 const PERIOD_REGEX = /(\w+ \d{1,2}, \d{4}) ?- ?(\w+ \d{1,2}, \d{4})/;
 
 export function isGLFormat(textShards: TextShard[][]): boolean {
-  //
+  const lines = getTextShardsAsLines(textShards);
+  // TODO: check if GL...
 }
